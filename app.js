0abaf23202c0f42add34d28fd01c0cc9fec9b8ec
PsychoGodly <90983397+PsychoGodly@users.noreply.github.com>
2024-06-28 15:38:57 +0100

    Add point modification, removal functionality, and history log

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let friends = [];
+let historyLog = [];
 
 function addFriend() {
     const friendName = document.getElementById('friendName').value;
@@ -10,8 +11,27 @@ function addFriend() {
 }
 
 function updatePoints(index, change) {
-    friends[index].points += change;
+    const friend = friends[index];
+    friend.points += change;
+    historyLog.push(`Updated ${friend.name}'s points by ${change} (total: ${friend.points})`);
     updateTable();
+    updateHistory();
+}
+
+function removeFriend(index) {
+    const friend = friends.splice(index, 1)[0];
+    historyLog.push(`Removed ${friend.name}`);
+    updateTable();
+    updateHistory();
+}
+
+function updatePointsManually(index, newValue) {
+    const friend = friends[index];
+    const oldValue = friend.points;
+    friend.points = newValue;
+    historyLog.push(`Manually updated ${friend.name}'s points from ${oldValue} to ${newValue}`);
+    updateTable();
+    updateHistory();
 }
 
 function updateTable() {
@@ -21,15 +41,25 @@ function updateTable() {
     friends.forEach((friend, index) => {
         const row = `<tr>
                         <td>${friend.name}</td>
-                        <td>${friend.points}</td>
+                        <td><input type="number" value="${friend.points}" onchange="updatePointsManually(${index}, parseInt(this.value))"></td>
                         <td>
                             <button onclick="updatePoints(${index}, 1)">+1</button>
                             <button onclick="updatePoints(${index}, 5)">+5</button>
                             <button onclick="updatePoints(${index}, -1)">-1</button>
                             <button onclick="updatePoints(${index}, -5)">-5</button>
+                            <button onclick="removeFriend(${index})">Remove</button>
                         </td>
                     </tr>`;
         tableBody.innerHTML += row;
+    });
+}
+
+function updateHistory() {
+    const historyList = document.getElementById('historyLog');
+    historyList.innerHTML = '';
+    historyLog.forEach(entry => {
+        const listItem = `<li>${entry}</li>`;
+        historyList.innerHTML += listItem;
     });
 }
 
